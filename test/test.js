2210a141450379469feffa02605fbee8be2855ae
John Otander <johnotander@gmail.com>
2015-02-06 16:16:06 -0700

    Initial commit

---
+++ test/test.js
@@ -0,0 +1,28 @@
+var assert = require('assert')
+var isCss = require('..')
+
+var cssPaths = [
+  'foo/file.css',
+  'src/foo/bar/file.CSS'
+]
+
+var nonCssPaths = [
+  'foo/filecss',
+  'file',
+  'file.scss'
+]
+
+describe('is-css', function() {
+
+  it('should return true for css files', function() {
+    cssPaths.forEach(function(cssFile) {
+      assert.ok(isCss(cssFile))
+    })
+  })
+
+  it('should return false for non css files', function() {
+    nonCssPaths.forEach(function(nonCssFile) {
+      assert.ok(!isCss(nonCssFile))
+    })
+  })
+})
